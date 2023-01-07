edc55f2d53b81e01f8b4a80f0021dd094e9808d9
Trevor <trevor.fontana@gmail.com>
2023-01-07 10:59:12 -0800

    Add files via upload

---
+++ .NET/GoogleAnalyticMetricDimensionRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.GoogleAnalytic
+{
+    public class GoogleAnalyticMetricDimensionRequest : GoogleAnalyticRequest
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        public List<string> Dimension { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        public List<string> Metric { get; set; }
+    }
+}
